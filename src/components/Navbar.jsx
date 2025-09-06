eb049a39ff260ecb0f7cd8d9fcd575c04459c47b
aleung30 <aleung30@student.ubc.ca>
2025-09-05 17:41:38 -0700

    created mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,81 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { HiMenu } from "react-icons/hi";
 import { IoClose } from "react-icons/io5";
 import { useState } from "react";
 
 export const Navbar = () => {
-   const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
 
-    return <section>
-        <div className="hidden md:flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link to="./" className="text-xl text-black">Logo</Link>
+  return (
+    <section>
+      <div className="flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link to="./" className="text-xl text-black">
+          Logo
+        </Link>
 
-        <ul className="flex">
-        <li className="px-5">
-                <Link to= "./review" className="text-xl">write a review</Link>
-            </li>
-            <button className="bg-[#F1CCCC] rounded-[3px] mx-5 items-center">
+        {/* desktop menu */}
 
+        <ul className="hidden md:flex items-center">
+          <li className="px-5">
+            <Link to="./review" className="text-xl">
+              write a review
+            </Link>
+          </li>
+          <button className="bg-[#F1CCCC] rounded-[3px] mx-5 items-center">
             <li className="px-5">
-                <Link to= "./login">login</Link>
+              <Link to="./login" className="text-xl">
+                login
+              </Link>
             </li>
-            </button>
+          </button>
 
-            <button className="bg-[#FADEDA] rounded-[3px] items-center">
+          <button className="bg-[#FADEDA] rounded-[3px] items-center">
             <li className="px-5">
-                <Link to= "./login" className="text-xl">sign up</Link>
+              <Link to="./register" className="text-xl">
+                sign up
+              </Link>
             </li>
-            </button>
-         
+          </button>
         </ul>
 
+        {/* mobile menu button */}
+        <div className="md:hidden">
+
+        <button onClick={() => setMenuOpen(!menuOpen)}>
+            {menuOpen ? <IoClose /> : <HiMenu className="text-3l" />}</button>
         </div>
+      </div>
+
+      {/* dropdown menu */}
+      {menuOpen && (
+        <ul className="flex flex-col items-center gap-4 md:hidden">
+             <li className="px-5">
+            <Link to="./review" onClick={() => setMenuOpen(false)}>
+              write a review
+            </Link>
+          </li>
+          <button className="bg-[#F1CCCC] rounded-[3px] mx-5 items-center">
+            <li className="px-5">
+            <Link to="./login" onClick={() => setMenuOpen(false)}>
+                login
+              </Link>
+            </li>
+          </button>
+
+          <button className="bg-[#FADEDA] rounded-[3px] items-center">
+            <li className="px-5">
+            <Link to="./register" onClick={() => setMenuOpen(false)}>
+                sign up
+              </Link>
+            </li>
+            </button>
+
+        </ul>
+      )}
+
+
     </section>
-}
+  );
+};

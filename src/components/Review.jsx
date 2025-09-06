8826a424485337e1ff5a49d234d99be91b61571f
aleung30 <aleung30@student.ubc.ca>
2025-09-05 20:27:26 -0700

    review

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import React from 'react'
-
-export const Review = () => {
-    
-=======
 
 
 export const Review = () => {
     return (
         <h1>review</h1>
     )
->>>>>>> 3a7ce8227089fe2c9dd2bfa5cea3bad12e4d94f5
 }

344ecd3652302ca8def657c6d24c6397e34a51d8
Rio <riomaruyama@hotmail.com>
2025-09-05 17:49:30 -0700

    changes bg colour

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,11 @@ import matchabowl from "../assets/matchabowl.png"
 
 export const Home = () => {
     return (
-        <div className="bg-green-50 h-screen flex items-center justify-center">
+        <div className="bg-[#ECF0E7] h-screen flex items-center justify-center">
             <div className='text-center z-index-100'>
-                <h2 className='font-extrabold text-7xl text-lime-900'>Find Your Matcha</h2>
+                <h2 className='font-extrabold text-7xl text-[#37532B]'>Find Your Matcha</h2>
                 <br/>
-                <p className='font-medium text-lime-900 text-3xl'>sign up to begin</p>
+                <p className='font-medium text-[#37532B] text-3xl'>sign up to begin</p>
             </div>
             <div className='z-index-99'>
                 <img src={leaves}/>

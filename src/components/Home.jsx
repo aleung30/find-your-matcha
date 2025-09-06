813b16a5fa053e7aca7e6112848bd507cbec852a
Rio <riomaruyama@hotmail.com>
2025-09-05 17:46:07 -0700

    added images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,13 @@ import matchabowl from "../assets/matchabowl.png"
 export const Home = () => {
     return (
         <div className="bg-green-50 h-screen flex items-center justify-center">
-            <div className='text-center'>
+            <div className='text-center z-index-100'>
                 <h2 className='font-extrabold text-7xl text-lime-900'>Find Your Matcha</h2>
                 <br/>
                 <p className='font-medium text-lime-900 text-3xl'>sign up to begin</p>
+            </div>
+            <div className='z-index-99'>
+                <img src={leaves}/>
             </div>
         </div>
     )

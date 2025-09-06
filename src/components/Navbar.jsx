677bed7227b877667ef1866e2fa35efe191d5e36
aleung30 <aleung30@student.ubc.ca>
2025-09-05 17:15:08 -0700

    Updated imports

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,26 +2,15 @@ import React from "react";
 import {Link} from "react-router-dom";
 import { HiMenu } from "react-icons/hi";
 import { IoClose } from "react-icons/io5";
+import { useState } from "react";
 
 export const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    
-    const toggleMenu = () => {
-        setIsOpen(!isOpen)
-    }
-
+   const [menuOpen, setMenuOpen] = useState(false);
 
 
     return <section>
-
-{!isOpen && (
-            <div className="cursor-pointer md:hidden"> 
-            <HiMenu size={30}/>
-            </div>
-        )}
         <div className="hidden md:flex flex-wrap items-center justify-between mx-auto p-4">
         <Link to="./" className="text-xl text-black">Logo</Link>
-
 
         <ul className="flex">
         <li className="px-5">

93c149364fc38ba52e2a3b2326a5c7ee1d7cd0c3
aleung30 <aleung30@student.ubc.ca>
2025-09-05 18:22:37 -0700

    Updated responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,12 +18,14 @@ export const Navbar = () => {
         {/* desktop menu */}
 
         <ul className="hidden md:flex items-center">
+            <button className="bg-[#D9D9D9] rounded-[10px] items-center">
           <li className="px-5">
             <Link to="./review" className="text-xl">
               write a review
             </Link>
           </li>
-          <button className="bg-[#F1CCCC] rounded-[3px] mx-5 items-center">
+          </button>
+          <button className="bg-[#F1CCCC] rounded-[10px] mx-5 items-center">
             <li className="px-5">
               <Link to="./login" className="text-xl">
                 login
@@ -31,7 +33,7 @@ export const Navbar = () => {
             </li>
           </button>
 
-          <button className="bg-[#FADEDA] rounded-[3px] items-center">
+          <button className="bg-[#FADEDA] rounded-[10px] items-center">
             <li className="px-5">
               <Link to="./register" className="text-xl">
                 sign up
@@ -39,34 +41,36 @@ export const Navbar = () => {
             </li>
           </button>
         </ul>
-
         {/* mobile menu button */}
         <div className="md:hidden">
 
         <button onClick={() => setMenuOpen(!menuOpen)}>
-            {menuOpen ? <IoClose /> : <HiMenu className="text-3l" />}</button>
+            {menuOpen ? <IoClose /> : <HiMenu/>}</button>
         </div>
       </div>
 
       {/* dropdown menu */}
       {menuOpen && (
-        <ul className="flex flex-col items-center gap-4 md:hidden">
-             <li className="px-5">
-            <Link to="./review" onClick={() => setMenuOpen(false)}>
+        <ul className="bg-white absolute flex w-full flex-col items-center gap-6 md:hidden rounded-2xl">
+                        <button className="bg-[#D9D9D9] rounded-[10px] items-center p-[3px] mt-10">
+
+                        <li className="px-5">
+            <Link to="./review" className="text-xl text-center" onClick={() => setMenuOpen(false)}>
               write a review
             </Link>
           </li>
-          <button className="bg-[#F1CCCC] rounded-[3px] mx-5 items-center">
+          </button>
+          <button className="bg-[#F1CCCC] rounded-[10px] p-[3px] items-center">
             <li className="px-5">
-            <Link to="./login" onClick={() => setMenuOpen(false)}>
+            <Link to="./login" className="text-xl" onClick={() => setMenuOpen(false)}>
                 login
               </Link>
             </li>
           </button>
 
-          <button className="bg-[#FADEDA] rounded-[3px] items-center">
+          <button className="bg-[#FADEDA] rounded-[10px] p-[3px] items-center mb-2">
             <li className="px-5">
-            <Link to="./register" onClick={() => setMenuOpen(false)}>
+            <Link to="./register" className="text-xl" onClick={() => setMenuOpen(false)}>
                 sign up
               </Link>
             </li>
